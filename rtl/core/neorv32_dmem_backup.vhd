886db5e852abff24402721f9379b258bb47c4cbe
monideepbora <monideepbora@gmail.com>
2021-03-07 11:41:06 +0100

    reverted the IMEM size

--- rtl/core/neorv32_dmem_backup.vhd
+++ rtl/core/neorv32_dmem_backup.vhd
@@ -1,9 +1,11 @@
 -- #################################################################################################
 -- # << NEORV32 - Processor-internal data memory (DMEM) >>                                         #
 -- # ********************************************************************************************* #
+-- # This version is intended for SIMULATION ONLY!                                                 #
+-- # ********************************************************************************************* #
 -- # BSD 3-Clause License                                                                          #
 -- #                                                                                               #
--- # Copyright (c) 2021, Stephan Nolting. All rights reserved.                                     #
+-- # Copyright (c) 2020, Stephan Nolting. All rights reserved.                                     #
 -- #                                                                                               #
 -- # Redistribution and use in source and binary forms, with or without modification, are          #
 -- # permitted provided that the following conditions are met:                                     #
@@ -42,7 +44,7 @@ use neorv32.neorv32_package.all;
 entity neorv32_dmem is
   generic (
     DMEM_BASE : std_ulogic_vector(31 downto 0) := x"80000000"; -- memory base address
-    DMEM_SIZE : natural := 8*1024  -- processor-internal instruction memory size in bytes
+    DMEM_SIZE : natural := 4*1024  -- processor-internal instruction memory size in bytes
   );
   port (
     clk_i  : in  std_ulogic; -- global clock line
@@ -69,32 +71,8 @@ architecture neorv32_dmem_rtl of neorv32_dmem is
   signal addr   : std_ulogic_vector(index_size_f(DMEM_SIZE/4)-1 downto 0);
 
   -- RAM --
-  -- The memory is built from 4x byte-wide memories defined as unique signals, since many synthesis tools
-  -- have problems with 32-bit memories with byte-enable signals or with multi-dimensional arrays.
-  type dmem_file_t is array (0 to DMEM_SIZE/4-1) of std_ulogic_vector(7 downto 0);
-  signal dmem_file_ll : dmem_file_t;
-  signal dmem_file_lh : dmem_file_t;
-  signal dmem_file_hl : dmem_file_t;
-  signal dmem_file_hh : dmem_file_t;
-
-
-  -- -------------------------------------------------------------------------------- --
-  -- attributes - these are *NOT mandatory*; just for footprint / timing optimization --
-  -- -------------------------------------------------------------------------------- --
-
-  -- lattice radiant --
-  attribute syn_ramstyle : string;
-  attribute syn_ramstyle of dmem_file_ll : signal is "no_rw_check";
-  attribute syn_ramstyle of dmem_file_lh : signal is "no_rw_check";
-  attribute syn_ramstyle of dmem_file_hl : signal is "no_rw_check";
-  attribute syn_ramstyle of dmem_file_hh : signal is "no_rw_check";
-
-  -- intel quartus prime --
-  attribute ramstyle : string;
-  attribute ramstyle of dmem_file_ll : signal is "no_rw_check";
-  attribute ramstyle of dmem_file_lh : signal is "no_rw_check";
-  attribute ramstyle of dmem_file_hl : signal is "no_rw_check";
-  attribute ramstyle of dmem_file_hh : signal is "no_rw_check";
+  type dmem_file_t is array (0 to DMEM_SIZE/4-1) of std_ulogic_vector(31 downto 0);
+  shared variable dmem_file_sim : dmem_file_t;
 
 begin
 
@@ -115,23 +93,20 @@ begin
         -- write --
         if (wren_i = '1') then
           if (ben_i(0) = '1') then
-            dmem_file_ll(to_integer(unsigned(addr))) <= data_i(07 downto 00);
+            dmem_file_sim(to_integer(unsigned(addr)))(07 downto 00) := data_i(07 downto 00);
           end if;
           if (ben_i(1) = '1') then
-            dmem_file_lh(to_integer(unsigned(addr))) <= data_i(15 downto 08);
+            dmem_file_sim(to_integer(unsigned(addr)))(15 downto 08) := data_i(15 downto 08);
           end if;
           if (ben_i(2) = '1') then
-            dmem_file_hl(to_integer(unsigned(addr))) <= data_i(23 downto 16);
+            dmem_file_sim(to_integer(unsigned(addr)))(23 downto 16) := data_i(23 downto 16);
           end if;
           if (ben_i(3) = '1') then
-            dmem_file_hh(to_integer(unsigned(addr))) <= data_i(31 downto 24);
+            dmem_file_sim(to_integer(unsigned(addr)))(31 downto 24) := data_i(31 downto 24);
           end if;
         end if;
         -- read --
-        rdata(07 downto 00) <= dmem_file_ll(to_integer(unsigned(addr)));
-        rdata(15 downto 08) <= dmem_file_lh(to_integer(unsigned(addr)));
-        rdata(23 downto 16) <= dmem_file_hl(to_integer(unsigned(addr)));
-        rdata(31 downto 24) <= dmem_file_hh(to_integer(unsigned(addr)));
+        rdata <= dmem_file_sim(to_integer(unsigned(addr)));
       end if;
     end if;
   end process dmem_file_access;
